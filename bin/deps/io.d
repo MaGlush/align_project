544a5e28e8e6f7e6931e9e289292a30e2b2b3c08
SoleSensei <dimargfd@gmail.com>
2017-10-23 01:24:40 +0300

    formalization add maked files

---
+++ bin/deps/io.d
@@ -0,0 +1,4 @@
+../bin/obj/io.o ../bin/deps/io.d: src/io.cpp include/mvc.h \
+ include/matrix.h include/matrix.hpp bridge/include/EasyBMP.h \
+ bridge/include/EasyBMP_DataStructures.h bridge/include/EasyBMP_BMP.h \
+ bridge/include/EasyBMP_VariousBMPutilities.h include/plugin.hpp
---
+++ bin/deps/matrix_example.d
@@ -0,0 +1,5 @@
+../bin/obj/matrix_example.o ../bin/deps/matrix_example.d: \
+ src/matrix_example.cpp include/matrix.h include/matrix.hpp include/io.h \
+ bridge/include/EasyBMP.h bridge/include/EasyBMP_DataStructures.h \
+ bridge/include/EasyBMP_BMP.h \
+ bridge/include/EasyBMP_VariousBMPutilities.h
